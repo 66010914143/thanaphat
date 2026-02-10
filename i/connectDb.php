697eef7a7c3b72534aacd01c8c17aa39f98117fa
66010914143 <66010914143@msu.ac.th>
2026-02-10 12:22:16 +0700

    Update connectDb.php

--- i/connectDb.php
+++ i/connectDb.php
@@ -1,9 +1,10 @@
 <?php
                 $host ="localhost";  
                 $user = "root";
-                $pwd = "123546789";
+                $pwd = "123456789";
                 $DB = "4143"; 
                 #เชื่อมต่อฐานข้อมูล
                 $conn = mysqli_connect($host,$user,$pwd,$DB) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
                 mysqli_query($conn, "SET NAMES utf8");
+
 ?>
