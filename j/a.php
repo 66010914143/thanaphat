7f9ecfadfd7ef0db5fba8a7e7a7fd618fab29a5d
66010914143 <66010914143@msu.ac.th>
2026-02-23 18:45:31 +0700

    Add files via upload

---
+++ j/a.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ธนภัทร อุปสีดา(ออย)</title>
+</head>
+
+<body>
+    <h1>งาน i</h1>
+    <h1>ธนภัทร อุปสีดา(ออย)</h1>
+<form method = "post" action="">
+    ชื่อภาค <input type= "text" name="rname" autofocus required>
+    <button type ="Submit" name = "Submit"> บันทึก </button>
+</FROM>
+<br>
+<br>
+
+<?php
+    if(isset($_POST['Submit'])){
+        include_once("connectDB.php");
+        $rname = $_POST['rname'];
+        $sql2 = "INSERT INTO `regions` VALUES (NULL, '{$_POST['rname']}')";
+        mysqli_query($conn,$sql2) or die ("insert ไม่ได้");
+    }
+
+?>
+<table border= "1">
+    <tr>
+        <th>รหัสภาค</th>
+        <th>ชื่อภาค</th> 
+        <th>ลบ</th>
+    </tr>
+<?php
+    include_once("connectDB.php");
+    $sql = "SELECT * FROM `regions` ORDER BY `r_id` ASC";
+    $rs = mysqli_query($conn , $sql); 
+
+    while($data = mysqli_fetch_array($rs)){
+?>
+    <tr>
+        <td><?php echo $data['r_id']; ?></td>
+        <td><?php echo $data['r_name']; ?></td>
+        <td width="50" align="center">
+            <a href="delete_regions.php?id=<?php echo $data['r_id']; ?>" 
+               onclick="return confirm('ยืนยันการลบ');">
+                <img src="images/bin.jpg" width="20">
+            </a>
+        </td>
+    </tr>
+
+<?php } ?> 
+
+
+</table>
+
+</body>
+</html>
